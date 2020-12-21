a6d6f80a8dd603de6e20f2a7fc25f158a1385fd0
leiqq <lqq213@126.com>
2020-12-21 15:49:52 +0800

    transport服务增加swagger

--- mic_transport/src/main/java/com/lei/transport/controller/TransportController.java
+++ mic_transport/src/main/java/com/lei/transport/controller/TransportController.java
@@ -1,7 +1,8 @@
 package com.lei.transport.controller;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import feign.OrderFeign;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/trans")
+@Api(tags = "transport初始测试相关接口")
 public class TransportController {
 
     @Autowired
     private OrderFeign orderFeign;
 
+    @ApiOperation("获取订单接口")
     @GetMapping("/getOrder")
-    //@SentinelResource(value = "getOrder")
     public String getOrder() {
         return orderFeign.getOrder();
     }
 
+    @ApiOperation("获取trans接口")
     @GetMapping("/getTrans")
     public String getTrans() {
         return "执行了mic-transport服务的getTrans方法啦!";
